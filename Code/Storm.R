4e7723a80f2e22baac50bfd880cd16066230a77d
Daniel <davidr9708@gmail.com>
2021-07-09 17:58:03 -0500

    Data frame, almost done. Needed: -Convert CROPS and PROPERTIES -New columms for months and years
    PD: Some events don't have data.

--- Code/Storm.R
+++ Code/Storm.R
@@ -3,16 +3,29 @@ library(tidyverse)
 library(R.utils)
 library(lubridate)
 # File used
-## 
+##Downloading 
 url <- 'https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2'
 download.file(url, destfile = 'Raw_Data/StormData.csv.bz2')
 
+## Decompressing
 bunzip2(filename = 'Raw_Data/StormData.csv.bz2', 
         destname = 'Raw_Data/StormData.csv', remove = FALSE, skip = TRUE) 
-
+## Reading
 Storm_dat <- 
-  read_csv('Raw_Data/StormData.csv')
+  read.csv('Raw_Data/StormData.csv')
 
 # Wrangling  
-## Taking only year 2010
-  table(month(dmy_hms(Storm_dat$BGN_DATE)),year(dmy_hms(Storm_dat$BGN_DATE)))
+## Taking only ye
+
+Storm_dat %>%  select(CROPDMGEXP) %>% table() 
+  filter(year(dmy_hms(BGN_DATE)) %in% 2007:2011, !is.na(CROPDMGEXP)) %>%
+       select(PROPDMGEXP) %>% table() 
+
+Storm_dat %>%  
+  mutate(DBGN_DATE = dmy_hms(BGN_DATE)) 
+  filter(DBGN_DATE %in% 2007:2011)
+  select()       ) %>%
+  select(EVTYPE) %>% table()
+
+str(Storm_dat)
+
