59783cb79ba934eff7d59cd6f73d01f99a3a5b43
Daniel <davidr9708@gmail.com>
2021-07-09 20:16:45 -0500

    Data frame done: -Crops and properties calculated.

--- Code/Storm.R
+++ Code/Storm.R
@@ -17,15 +17,43 @@ Storm_dat <-
 # Wrangling  
 ## Taking only ye
 
-Storm_dat %>%  select(CROPDMGEXP) %>% table() 
-  filter(year(dmy_hms(BGN_DATE)) %in% 2007:2011, !is.na(CROPDMGEXP)) %>%
-       select(PROPDMGEXP) %>% table() 
+Wrangled_Data <-
+    Storm_dat %>%  
+      select(EVTYPE, BGN_DATE, INJURIES, FATALITIES, PROPDMG, PROPDMGEXP, 
+             CROPDMG, CROPDMGEXP) %>%
+      mutate(BGN_DATE = dmy_hms(BGN_DATE),
+             PROPDMGEXP = ifelse(PROPDMGEXP == 'K', 10^3, 
+                                 ifelse(PROPDMGEXP == 'M', 10^6, 10^9)
+                                 ),
+             CROPDMGEXP = ifelse(CROPDMGEXP == 'K', 10^3, 
+                                 ifelse(CROPDMGEXP == 'M', 10^6, 10^9)
+                                 ),
+             PROPDMG = PROPDMG*PROPDMGEXP,
+             CROPDMG = CROPDMG*CROPDMGEXP,
+             TOTAL_ECONOMICAL_LOSTS = CROPDMG+PROPDMG
+             )  %>% 
+      filter(year(BGN_DATE) %in% 2007:2011) 
+# Plotting
+## COSTS
+### CROPS
+Wrangled_Data %>% 
+  mutate(EVTYPE = reorder(EVTYPE, PROPDMG, sum)) %>%
+    ggplot(aes(y =EVTYPE, x= PROPDMG)) + 
+       geom_bar(stat="identity", fill = 'darkred') + 
+          ylab('') + xlab('') + 
+          ggtitle('ECONOMICAL LOSTS IN PROPERTIES (USD) PER EVENT')
+### CROPS
+Wrangled_Data %>% 
+  mutate(EVTYPE = reorder(EVTYPE, CROPDMG, sum)) %>%
+    ggplot(aes(y =EVTYPE, x= CROPDMG)) + 
+      geom_bar(stat="identity", fill = 'darkred') + 
+        ylab('') + xlab('') + 
+        ggtitle('ECONOMICAL LOSTS IN CROPS (USD) PER EVENT')
 
-Storm_dat %>%  
-  mutate(DBGN_DATE = dmy_hms(BGN_DATE)) 
-  filter(DBGN_DATE %in% 2007:2011)
-  select()       ) %>%
-  select(EVTYPE) %>% table()
-
-str(Storm_dat)
-
+### TOTAL
+Wrangled_Data %>% 
+  mutate(EVTYPE = reorder(EVTYPE, TOTAL_ECONOMICAL_LOSTS, sum)) %>%
+    ggplot(aes(y =EVTYPE, x= TOTAL_ECONOMICAL_LOSTS)) + 
+       geom_bar(stat="identity", fill = 'darkred') + 
+           ylab('') + xlab('') + 
+           ggtitle('TOTAL ECONOMICAL LOSTS (USD) PER EVENT')
